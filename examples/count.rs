fcfcdc8f29461bc15be65e929ddddc5300078d79
wverhoef <wilco@verhoef.nu>
2026-03-27 22:22:37 +0100

    Update count example

--- examples/count.rs
+++ examples/count.rs
@@ -12,7 +12,7 @@ extern crate osmpbfreader;
 
 fn count<F: Fn(&osmpbfreader::Tags) -> bool>(filter: F, filename: &std::ffi::OsStr) {
     let r = std::fs::File::open(std::path::Path::new(filename)).unwrap();
-    let mut pbf = osmpbfreader::OsmPbfReader::new(r);
+    let mut pbf = osmpbfreader::ParOsmPbfReader::new(r);
     let mut nb_nodes = 0;
     let mut sum_lon = 0.;
     let mut sum_lat = 0.;
@@ -20,7 +20,7 @@ fn count<F: Fn(&osmpbfreader::Tags) -> bool>(filter: F, filename: &std::ffi::OsS
     let mut nb_way_nodes = 0;
     let mut nb_rels = 0;
     let mut nb_rel_refs = 0;
-    for obj in pbf.par_iter().map(Result::unwrap) {
+    for obj in pbf.iter().map(Result::unwrap) {
         if !filter(obj.tags()) {
             continue;
         }
